f7f2f56db5da3d0b865997a4e97eec0d093dad34
davidsantosmerino <dsantosmerino92@gmail.com>
2016-04-06 16:13:37 +0200

    add spec structure and first test

---
+++ lib/filmaffinity/movie.rb
@@ -0,0 +1,15 @@
+module FilmAffinity
+  class Movie
+    def initialize id
+      @id = id
+    end
+
+    def cast
+      ["Jim Carrey",
+      "Laura Linney",
+      "Noah Emmerich",
+      "Ed Harris"]
+    end
+
+  end
+end
---
+++ spec/filmaffinity/movie_spec.rb
@@ -0,0 +1,22 @@
+require ".././spec_helper"
+
+describe "FilmAffinity::Movie" do
+  describe "valid movie" do
+    # Get The Truman Show (1998)
+    subject(:movie) { FilmAffinity::Movie.new('504889') }
+
+    it "#cast" do
+      cast = movie.cast
+      expected_cast = [
+        "Jim Carrey",
+        "Laura Linney",
+        "Noah Emmerich",
+        "Ed Harris"
+      ]
+
+      expect(cast).to be_an(Array)
+      expect(cast).to include(*expected_cast)
+    end
+
+  end
+end
