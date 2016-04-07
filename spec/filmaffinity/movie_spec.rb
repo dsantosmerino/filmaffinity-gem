9833aa805f32e9d937fbeb749893fc7e48a8245c
davidsantosmerino <dsantosmerino92@gmail.com>
2016-04-07 17:26:16 +0200

    add movie specs

--- spec/filmaffinity/movie_spec.rb
+++ spec/filmaffinity/movie_spec.rb
@@ -1,22 +1,106 @@
 require_relative "../spec_helper"
 
 describe "FilmAffinity::Movie" do
-  describe "valid movie" do
-    # Get The Truman Show (1998)
-    subject(:movie) { FilmAffinity::Movie.new("504889", "El show de Truman (Una vida en directo)") }
 
-    it "#cast" do
-      cast = movie.cast
+  describe "#rating" do
+    subject(:movie) { FilmAffinity::Movie.new(504889, "El show de Truman (Una vida en directo)") }
+    it "should return an Float" do
+      expect(movie.rating).to be_a(Float)
+    end
+  end
+
+  describe "#year" do
+    subject(:movie) { FilmAffinity::Movie.new(504889, "El show de Truman (Una vida en directo)") }
+    it "should return an Fixnum" do
+      expect(movie.year).to be_an(Fixnum)
+    end
+    it "should return 1998" do
+      expect(movie.year).to eq(1998)
+    end
+  end
+
+  describe "#duration" do
+    subject(:movie) { FilmAffinity::Movie.new(504889, "El show de Truman (Una vida en directo)") }
+    it "should return an Fixnum" do
+      expect(movie.duration).to be_a(Fixnum)
+    end
+    it "should return 103" do
+      expect(movie.duration).to eq(103)
+    end
+  end
+
+  describe "#country" do
+    subject(:movie) { FilmAffinity::Movie.new(504889, "El show de Truman (Una vida en directo)") }
+    it "should return a String" do
+      expect(movie.country).to be_a(String)
+    end
+    it "should return 'Estados Unidos'" do
+      expect(movie.country).to eq("Estados Unidos")
+    end
+  end
+
+  describe "#director" do
+    subject(:movie) { FilmAffinity::Movie.new(504889, "El show de Truman (Una vida en directo)") }
+    it "should return a String" do
+      expect(movie.director).to be_a(String)
+    end
+    it "should return 'Peter Weir'" do
+      expect(movie.director).to eq("Peter Weir")
+    end
+  end
+
+  describe "#script" do
+    subject(:movie) { FilmAffinity::Movie.new(504889, "El show de Truman (Una vida en directo)") }
+    it "should return a String" do
+      expect(movie.script).to be_a(String)
+    end
+    it "should return 'Andrew Niccol'" do
+      expect(movie.script).to eq("Andrew Niccol")
+    end
+  end
+
+  describe "#cast" do
+    subject(:movie) { FilmAffinity::Movie.new(504889, "El show de Truman (Una vida en directo)") }
+
+    it "should return an Array" do
+      expect(movie.cast).to be_an(Array)
+    end
+    it "should to include the passed cast" do
       expected_cast = [
         "Jim Carrey",
         "Laura Linney",
         "Noah Emmerich",
         "Ed Harris"
       ]
-
-      expect(cast).to be_an(Array)
-      expect(cast).to include(*expected_cast)
+      expect(movie.cast).to include(*expected_cast)
     end
-
   end
+
+  describe "#company" do
+    subject(:movie) { FilmAffinity::Movie.new(504889, "El show de Truman (Una vida en directo)") }
+
+    it "should return a String" do
+      expect(movie.company).to be_a(String)
+    end
+    it "should return 'Paramount Pictures / Scott Rudin Productions'" do
+      expect(movie.company).to eq("Paramount Pictures / Scott Rudin Productions")
+    end
+  end
+
+  describe "#genres" do
+    subject(:movie) { FilmAffinity::Movie.new(504889, "El show de Truman (Una vida en directo)") }
+
+    it "should return an Array" do
+      expect(movie.genres).to be_an(Array)
+    end
+    it "should to include the passed genres" do
+      expected_genres = [
+        "Drama",
+        "Comedia",
+        "Sátira"
+      ]
+      expect(movie.genres).to include(*expected_genres)
+    end
+  end
+
 end
