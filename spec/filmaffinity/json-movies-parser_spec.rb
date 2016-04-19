01ce1e6e9fcfda0e8529b43202dc98349e221cb0
Oriol Bellido <obelli89@gmail.com>
2016-04-19 12:50:01 +0200

    Add rubocop

--- spec/filmaffinity/json-movies-parser_spec.rb
+++ spec/filmaffinity/json-movies-parser_spec.rb
@@ -1,13 +1,13 @@
-require_relative "../spec_helper"
+require_relative '../spec_helper'
 
-describe "JsonMoviesParser" do
+describe 'JsonMoviesParser' do
 
-  describe "#to_hashes" do
+  describe '#to_hashes' do
     subject { JsonMoviesParser.new }
 
-    it "return some valid ids for a given search" do
-      hashes = subject.to_hashes FilmAffinity::Search.new("truman").movies
-      expect(hashes.map {|hash| hash["id"]}).to include(193232, 504889)
+    it 'return some valid ids for a given search' do
+      hashes = subject.to_hashes FilmAffinity::Search.new('truman').movies
+      expect(hashes.map { |hash| hash['id'] }).to include(193_232, 504_889)
     end
 
   end
