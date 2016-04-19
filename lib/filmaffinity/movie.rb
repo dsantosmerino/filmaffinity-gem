2eed4843f3e12164f26bfb9a9e71aa80f870efb9
davidsantosmerino <dsantosmerino92@gmail.com>
2016-04-19 23:29:44 +0200

    add rescues

--- lib/filmaffinity/movie.rb
+++ lib/filmaffinity/movie.rb
@@ -17,46 +17,46 @@ module FilmAffinity
     end
 
     def title
-      @title ||= document_html.at(Constants.tag(:title)).content.strip
+      @title ||= document_html.at(Constants.tag(:title)).content.strip rescue nil
     end
 
     def year
-      document_html.at(Constants.tag(:year)).content[/\d+/].to_i
+      document_html.at(Constants.tag(:year)).content[/\d+/].to_i rescue nil
     end
 
     def duration
-      document_html.at(Constants.tag(:duration)).content[/\d+/].to_i
+      document_html.at(Constants.tag(:duration)).content[/\d+/].to_i rescue nil
     end
 
     def country
-      raw_country = document_html.at(Constants.tag(:country)).next_sibling.content
-      raw_country.gsub(/\A[[:space:]]+|[[:space:]]+\z/, '')
+      raw_country = document_html.at(Constants.tag(:country)).next_sibling.content rescue nil
+      raw_country.gsub(/\A[[:space:]]+|[[:space:]]+\z/, '') if raw_country
     end
 
     def director
-      raw_director = document_html.at(Constants.tag(:director)).content
-      raw_director.strip
+      raw_director = document_html.at(Constants.tag(:director)).content rescue nil
+      raw_director.strip if raw_director
     end
 
     def music
-      document_html.at(Constants.tag(:music)).next_sibling.next_sibling.content
+      document_html.at(Constants.tag(:music)).next_sibling.next_sibling.content rescue nil
     end
 
     def company
-      document_html.at(Constants.tag(:company)).next_sibling.next_sibling.content
+      document_html.at(Constants.tag(:company)).next_sibling.next_sibling.content rescue nil
     end
 
     def script
-      document_html.at(Constants.tag(:script)).next_sibling.next_sibling.content
+      document_html.at(Constants.tag(:script)).next_sibling.next_sibling.content rescue nil
     end
 
     def photography
-      document_html.at(Constants.tag(:photography)).next_sibling.next_sibling.content
+      document_html.at(Constants.tag(:photography)).next_sibling.next_sibling.content rescue nil
     end
 
     def cast
       actors = []
-      node = document_html.search(Constants.tag(:cast))
+      node = document_html.search(Constants.tag(:cast)) rescue []
       node.each do |actor|
         actors << actor.at(Constants.tag(:cast_each)).content.strip
       end
@@ -65,7 +65,7 @@ module FilmAffinity
 
     def genres
       genres = []
-      node = document_html.at(Constants.tag(:genre)).next_sibling.next_sibling
+      node = document_html.at(Constants.tag(:genre)).next_sibling.next_sibling rescue []
       raw_genres = node.search("a")
       raw_genres.each do |raw_genre|
         genres << raw_genre.content.strip
@@ -74,25 +74,22 @@ module FilmAffinity
     end
 
     def sinopsis
-      document_html.at(Constants.tag(:sinopsis)).content
-    end
-
-    def prizes
+      document_html.at(Constants.tag(:sinopsis)).content rescue nil
     end
 
     def rating
-      raw_rating = document_html.at(Constants.tag(:rating)).content.strip
-      raw_rating.tr(",", ".").to_f
+      raw_rating = document_html.at(Constants.tag(:rating)).content.strip rescue nil
+      raw_rating.tr(",", ".").to_f if raw_rating
     end
 
     def poster
-      poster_url = document_html.at(Constants.tag(:poster))["src"]
-      @poster_manager.load_poster(poster_url)
+      poster_url = document_html.at(Constants.tag(:poster))["src"] rescue nil
+      @poster_manager.load_poster(poster_url) if poster_url
     end
 
     def poster_big
-      poster_url = document_html.at(Constants.tag(:poster_big))["href"]
-      @poster_manager.load_poster(poster_url)
+      poster_url = document_html.at(Constants.tag(:poster_big))["href"] rescue nil
+      @poster_manager.load_poster(poster_url) if poster_url
     end
 
     def to_json
