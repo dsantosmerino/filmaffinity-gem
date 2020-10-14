536610edeeb9d00625460c85862b15c473327455
David Santos Merino <dsantosmerino@gmail.com>
2020-10-14 21:54:36 +0200

    Fix missing configuration reset
    The previous setup was assuming that previously executed tests changed the main configuration making some requests and expectations in Spanish.

--- spec/filmaffinity/search_spec.rb
+++ spec/filmaffinity/search_spec.rb
@@ -8,13 +8,13 @@ describe FilmAffinity::Search, :vcr do
       movies = search.movies
       expect(movies).to be_an(Array)
     end
-    it 'returns just FilmAffinity::Movie objects only' do
+    it 'returns FilmAffinity::Movie objects only' do
       expect(search.movies).to all(be_an(FilmAffinity::Movie))
     end
-    it 'includes the "Truman Show"' do
+    it 'includes the "The Truman Show"' do
       movies = search.movies
       truman_movie = FilmAffinity::Movie.new(
-        504_889, 'El show de Truman (Una vida en directo)'
+        504_889, 'The Truman Show'
       )
       expect(movies).to include_movie(truman_movie)
     end
@@ -30,7 +30,7 @@ describe FilmAffinity::Search, :vcr do
     end
 
     it 'parses the title' do
-      title = 'El show de Truman (Una vida en directo)  (1998)'
+      title = 'The Truman Show  (1998)'
       expect(title).to eq(search.movies.first.title)
     end
   end
